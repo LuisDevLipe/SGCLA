c1474d8409218e720f7ad1a778085585366ae0c3
LuisDevLipe <luisfelipebsj@gmail.com>
2023-11-12 22:28:11 -0300

    views / controllers: controladores de rota;

--- SGCLA/SGCLAapp/urls.py
+++ SGCLA/SGCLAapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "SGCLA"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("detalhes/<int:livro_id>",views.detalhes, name="detalhes"),
-    path("<str:busca>/", views.buscaDeLivros, name="busca")
+    path("<int:livro_id>/detalhes/" ,views.detalhes, name="detalhes"),
+    path("resultadoDaBusca/<str:busca>", views.resultadoDaBusca, name="buscar"),
+    path("adicionarUmLivroRota/", views.adicionarUmLivroRota, name="adicionarUmLivroRota"),
+    path("adicionarUmLivro/", views.adicionarUmLivro, name="adicionarUmLivro")
 ]
--- SGCLA/SGCLAapp/views.py
+++ SGCLA/SGCLAapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404, HttpResponseRedirect
 from django.utils import timezone
-from .models import Livro
+from .models import Livro, LivrosAlugados, Usuario
+from django.urls import reverse
+import datetime
 from django.template import loader
 
 
@@ -17,13 +19,61 @@ def index(request):
 
 def detalhes(request,livro_id):
     livro = get_object_or_404(Livro, pk = livro_id)
-    return render(request, "SGCLAapp/detalhe.html",{
-        "livro": livro
+    alugados = LivrosAlugados.objects.filter(livro_id = livro_id)
+    livrosDisponiveis = livro.unidades - alugados.count()
+    return render(request, "SGCLAapp/detalhes.html",{
+        "livro": livro,
+        "alugados": alugados,
+        "disponiveis":livrosDisponiveis,
     })
 
-def buscaDeLivros(request,busca):
-    livros = Livro.objects.filter(titulo__contains = busca)
-    saida = ", ".join([l.titulo + "-" + l.autor for l in livros])
-    return HttpResponse(
-        "Lista de livros pesquisados" + " Com o termo " + saida
+def resultadoDaBusca(request,busca):
+
+    busca = request.GET["titulo"]
+    livros = list(Livro.objects.filter(titulo__icontains = busca))
+    livros += Livro.objects.filter(autor__icontains = busca)
+
+    return render(
+        request,
+        "SGCLAapp/index.html",
+        {
+            "livros":livros,
+            "busca" : True
+        }
     )
+
+def adicionarUmLivroRota(request):
+    template = loader.get_template("SGCLAapp/adicionarUmLivro.html")
+    contexto = {"fakeIt_tillYouMakeIt" : True}
+    return  HttpResponse(template.render(contexto, request))
+
+def adicionarUmLivro(request):
+    if int(request.POST["unidades"]) <= 0:
+        raise ValueError("As unidades precisam ser um número válido maior que 0.")
+    
+    livro, inserido = Livro.objects.get_or_create(
+        titulo=request.POST["titulo"],
+        genero=request.POST["genero"],
+        autor=request.POST["autor"],
+        editora=request.POST["editora"],
+        anoPublicacao=request.POST["anoPublicacao"],      
+        unidades=int(request.POST["unidades"])
+    )
+
+    if inserido:
+        return HttpResponseRedirect(
+            reverse(
+                "SGCLA:detalhes",
+                kwargs={"livro_id":livro.id}
+            )
+        )
+    
+    else:
+        return render(
+                request,
+                "SGCLAapp/detalhe.html",
+                {
+                    "livro":livro,
+                    "mensagemDeErro":"Este livro já consta na base de dados"
+                },
+            )
