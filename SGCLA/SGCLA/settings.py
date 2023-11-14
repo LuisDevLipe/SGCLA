13c1e721a8da3a618df13f19b97ae0412643fcf9
LuisDevLipe <luisfelipebsj@gmail.com>
2023-11-14 09:46:26 -0300

    configurações de produção

--- SGCLA/SGCLA/settings.py
+++ SGCLA/SGCLA/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=dor+!7_geh_f$!(t9h=heq=^@1ejzcfb!4lqvydkcl6+j%+wz'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-=dor+!7_geh_f$!(t9h=heq=^@1ejzcfb!4lqvydkcl6+j%+wz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
